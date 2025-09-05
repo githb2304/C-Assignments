67fa275ad324f662fb2c526425fee0e8cf819485
ayush <github23043@gmail.com>
2025-09-05 18:53:05 +0530

    Initial commit

---
+++ day1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num1;
+    printf("enter first number :\n");
+    scanf("%d",&num1);
+    int num2;
+    printf("enter second number :\n");
+    scanf("%d",&num2);
+    printf("the sum is %d",num1+num2);
+    return 0;
+}
