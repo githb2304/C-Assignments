a9d249055641a2f819874e2512a791016bf8a70a
githb2304 <github23043@gmail.com>
2025-09-06 15:52:16 +0530

    Add files via upload

---
+++ day3_q5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float c;
+    printf("type temprature in celcius \n");
+    scanf("%f",&c);
+    float f;
+    f=c*9/5+32;
+    printf("%f",f);
+    return 0;
+}
---
+++ day3_q6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter a \n");
+    scanf("%d",&a);
+    int b;
+    printf("enter b \n");
+    scanf("%d",&b);
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    printf("a=%d b=%d",a,b);
+    return 0;
+
+}
