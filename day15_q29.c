72183159fbeef1b6932895ab4bb017812b1b8b26
githb2304 <github23043@gmail.com>
2025-09-25 20:27:12 +0530

    Add files via upload

---
+++ day15_q29.c
@@ -0,0 +1,17 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main()
+{
+ int n;
+ printf("enter the number whose factorial you want to claculate\n");
+ scanf("%d",&n);
+ int i=1;
+ int prd=1;
+ while (i<=n)
+ {
+    prd=prd*i;
+    i++;
+ }
+ printf("factorial of %d is %d",n,prd);
+ return 0;   
+}
---
+++ day15_q30.c
@@ -0,0 +1,18 @@
+//Write a program to reverse a given number.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter number\n");
+    scanf("%d",&n);
+    int revnum=0;
+    while (n!=0)
+    {
+        int digit = n%10;
+        revnum=revnum*10+digit;
+        n=n/10;
+    }
+    printf("reversed number is %d \n",revnum);
+    return 0;
+
+}
